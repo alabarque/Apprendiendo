d2361b2a3296f2ea4501f5e54089e4e1104e1a97
alabarque <agustinlabarc@hotmail.com>
2021-06-06 16:20:12 -0300

    first commit

---
+++ src/main/java/com/proyecto/apprendiendo/ApprendiendoApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.apprendiendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApprendiendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApprendiendoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/apprendiendo/ApprendiendoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.apprendiendo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprendiendoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
