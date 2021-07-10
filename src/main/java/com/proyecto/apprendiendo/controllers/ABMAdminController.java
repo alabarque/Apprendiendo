bbdd3e008c27198a4b8f8b273112a2e2ab9a821d
alabarque <agustinlabarc@hotmail.com>
2021-07-10 11:11:48 -0300

    agrego update y delete

--- src/main/java/com/proyecto/apprendiendo/controllers/ABMAdminController.java
+++ src/main/java/com/proyecto/apprendiendo/controllers/ABMAdminController.java
@@ -1,21 +1,22 @@
 package com.proyecto.apprendiendo.controllers;
 
 import com.proyecto.apprendiendo.entities.dtos.ClassroomDTO;
+import com.proyecto.apprendiendo.services.DeleteClassroomService;
 import com.proyecto.apprendiendo.services.GetClassroomService;
 import com.proyecto.apprendiendo.services.NewClassroomService;
+import com.proyecto.apprendiendo.services.UpdateClassroomService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @AllArgsConstructor
 public class ABMAdminController {
 
-    NewClassroomService newClassroomService;
-    GetClassroomService getClassroomService;
+    private NewClassroomService newClassroomService;
+    private GetClassroomService getClassroomService;
+    private DeleteClassroomService deleteClassroomService;
+    private UpdateClassroomService updateClassroomService;
 
     @PostMapping(path = "/class")
     public void newClassroom(@RequestBody ClassroomDTO classroomDTO){
@@ -25,5 +26,15 @@ public class ABMAdminController {
     @GetMapping (path = "/class/{id}")
     public ClassroomDTO getClassroom(@PathVariable Long idClass){
         return getClassroomService.execute(idClass);
+    }
+
+    @DeleteMapping(path = "/class/{id}")
+    public void deleteClassroom(@PathVariable Long classroomId){
+        deleteClassroomService.execute(classroomId);
+    }
+
+    @PutMapping(path = "/class")
+    public void updateClassroom(@RequestBody ClassroomDTO classroomDTO){
+        updateClassroomService.execute(classroomDTO);
     }
 }
