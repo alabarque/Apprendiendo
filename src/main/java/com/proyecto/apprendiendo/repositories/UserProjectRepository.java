d7d100daededbb099bd75c56f311e586263f003d
Javier Leonardo Soto <jlsoto85@gmail.com>
2021-08-13 16:59:31 -0300

    agregados metodos para busqueda de usuarios de proyectos y proyectos de usuarios

--- src/main/java/com/proyecto/apprendiendo/repositories/UserProjectRepository.java
+++ src/main/java/com/proyecto/apprendiendo/repositories/UserProjectRepository.java
@@ -1,10 +1,14 @@
 package com.proyecto.apprendiendo.repositories;
 
+import com.proyecto.apprendiendo.entities.User;
 import com.proyecto.apprendiendo.entities.UserProject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface UserProjectRepository extends JpaRepository<UserProject,Long> {
-    //User findByUsername(String username);
+    ArrayList<UserProject> findallByUserId(Long userId);
+    ArrayList<UserProject> findallByProjectId(Long projectId);
 }
