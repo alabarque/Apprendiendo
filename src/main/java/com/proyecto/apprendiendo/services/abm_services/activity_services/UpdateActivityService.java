a8e9d9e79de09860c72f9b88206e559a58103482
Javier Leonardo Soto <jlsoto85@gmail.com>
2021-08-26 19:45:16 -0300

    creados endpoints de ABM para activities

---
+++ src/main/java/com/proyecto/apprendiendo/services/abm_services/activity_services/UpdateActivityService.java
@@ -0,0 +1,26 @@
+package com.proyecto.apprendiendo.services.abm_services.activity_services;
+
+import com.proyecto.apprendiendo.entities.Activity;
+import com.proyecto.apprendiendo.entities.dtos.ActivityDTO;
+import com.proyecto.apprendiendo.repositories.ActivityRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@AllArgsConstructor
+@Transactional
+public class UpdateActivityService {
+
+    private ActivityRepository activityRepository;
+
+    public Long execute(ActivityDTO activityDTO){
+        Activity activity = activityRepository.getById(activityDTO.getId());
+        activity.setId(activityDTO.getId());
+        activity.setName(activityDTO.getName());
+        activity.setProjectId(activityDTO.getProjectId());
+        activityRepository.save(activity);
+        return activityDTO.getId();
+    }
+}
---
+++ src/main/java/com/proyecto/apprendiendo/services/mappers/ActivityMapper.java
@@ -0,0 +1,14 @@
+package com.proyecto.apprendiendo.services.mappers;
+
+import com.proyecto.apprendiendo.entities.Activity;
+import com.proyecto.apprendiendo.entities.dtos.ActivityDTO;
+
+public class ActivityMapper {
+    public static ActivityDTO entityToDto(Activity activity){
+        return ActivityDTO.builder()
+                        .id(activity.getId())
+                        .name(activity.getName())
+                        .projectId(activity.getProjectId())
+                        .build();
+    }
+}
