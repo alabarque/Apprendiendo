0425744967cea590838807ec1ce0adb4b961e6b5
Javier Leonardo Soto <jlsoto85@gmail.com>
2021-09-29 11:48:42 -0300

    agregado endpoint para vista de progreso de actividades

--- src/main/java/com/proyecto/apprendiendo/controllers/ActivityController.java
+++ src/main/java/com/proyecto/apprendiendo/controllers/ActivityController.java
@@ -1,14 +1,19 @@
 package com.proyecto.apprendiendo.controllers;
 
 import com.proyecto.apprendiendo.entities.dtos.ActivityDTO;
+import com.proyecto.apprendiendo.entities.dtos.StudentActivityDTO;
 import com.proyecto.apprendiendo.services.abm_services.activity_services.CreateActivityService;
 import com.proyecto.apprendiendo.services.abm_services.activity_services.DeleteActivityService;
 import com.proyecto.apprendiendo.services.abm_services.activity_services.GetActivityService;
 import com.proyecto.apprendiendo.services.abm_services.activity_services.UpdateActivityService;
+import com.proyecto.apprendiendo.services.abm_services.student_activity_services.GetActivityStudentsProgressService;
+import com.proyecto.apprendiendo.services.abm_services.student_activity_services.GetStudentActivityProgressService;
 import com.proyecto.apprendiendo.utils.ResponseDecorator;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @AllArgsConstructor
@@ -19,6 +24,7 @@ public class ActivityController {
     private DeleteActivityService deleteActivityService;
     private UpdateActivityService updateActivityService;
     private ResponseDecorator responseDecorator;
+    private GetActivityStudentsProgressService getActivityStudentsProgressService;
 
     @PostMapping(path = "activity")
     public ResponseEntity<Long> newActivity(@RequestBody ActivityDTO activityDTO){
@@ -38,5 +44,10 @@ public class ActivityController {
     @PutMapping(path = "activity")
     public ResponseEntity<Long> updateActivity(@RequestBody ActivityDTO activityDTO){
         return responseDecorator.decorate(()->updateActivityService.execute(activityDTO));
+    }
+
+    @GetMapping (path = "activity/{activityId}/progress")
+    public ResponseEntity<ArrayList<StudentActivityDTO>> getActivityProgress(@PathVariable Long activityId){
+        return responseDecorator.decorate(()->getActivityStudentsProgressService.execute(activityId));
     }
 }
