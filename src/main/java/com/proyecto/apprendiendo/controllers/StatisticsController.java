581680a34655cef2296e8251b0beaa59e386989c
Javier Leonardo Soto <jlsoto85@gmail.com>
2021-11-03 21:00:28 -0300

    agregados paths que faltaban para estadisticas de students

--- src/main/java/com/proyecto/apprendiendo/controllers/StatisticsController.java
+++ src/main/java/com/proyecto/apprendiendo/controllers/StatisticsController.java
@@ -39,6 +39,10 @@ public class StatisticsController {
         return responseDecorator.decorate(() -> getActivitiesStatisticsService.execute(teacherId,"TEACHER", "ACTIVITY"));
     }
 
+    @GetMapping(path = "statistics/teacher/{teacherId}/students")
+    public ResponseEntity<ArrayList<StatisticDTO>> getTeacherStudentsStatistics(@PathVariable Long teacherId) {
+        return responseDecorator.decorate(() -> getActivitiesStatisticsService.execute(teacherId,"TEACHER", "STUDENT"));
+    }
 
     @GetMapping(path = "statistics/classroom/{classroomId}/projects")
     public ResponseEntity<ArrayList<StatisticDTO>> getClassroomProjectsStatistics(@PathVariable Long classroomId) {
@@ -55,6 +59,10 @@ public class StatisticsController {
         return responseDecorator.decorate(() -> getActivitiesStatisticsService.execute(classroomId,"CLASSROOM", "ACTIVITY"));
     }
 
+    @GetMapping(path = "statistics/classroom/{classroomId}/students")
+    public ResponseEntity<ArrayList<StatisticDTO>> getClassroomStudentsStatistics(@PathVariable Long classroomId) {
+        return responseDecorator.decorate(() -> getActivitiesStatisticsService.execute(classroomId,"CLASSROOM", "STUDENT"));
+    }
 
     @GetMapping(path = "statistics/project/{projectId}/lessons")
     public ResponseEntity<ArrayList<StatisticDTO>> getProjectLessonsStatistics(@PathVariable Long projectId) {
@@ -66,12 +74,19 @@ public class StatisticsController {
         return responseDecorator.decorate(() -> getActivitiesStatisticsService.execute(projectId,"PROJECT", "ACTIVITY"));
     }
 
+    @GetMapping(path = "statistics/project/{projectId}/students")
+    public ResponseEntity<ArrayList<StatisticDTO>> getProjectStudentsStatistics(@PathVariable Long projectId) {
+        return responseDecorator.decorate(() -> getActivitiesStatisticsService.execute(projectId,"PROJECT", "STUDENT"));
+    }
 
     @GetMapping(path = "statistics/lesson/{lessonId}/activities")
     public ResponseEntity<ArrayList<StatisticDTO>> getLessonActivitiesStatistics(@PathVariable Long lessonId) {
         return responseDecorator.decorate(() -> getActivitiesStatisticsService.execute(lessonId,"LESSON", "ACTIVITY"));
     }
 
-
+    @GetMapping(path = "statistics/lesson/{lessonId}/students")
+    public ResponseEntity<ArrayList<StatisticDTO>> getLessonStudentsStatistics(@PathVariable Long lessonId) {
+        return responseDecorator.decorate(() -> getActivitiesStatisticsService.execute(lessonId,"LESSON", "STUDENT"));
+    }
 
 }
